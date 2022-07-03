37c6a296140244edbb694de55566c3047987b950
Swethana01 <105113322+Swethana01@users.noreply.github.com>
2022-07-03 15:49:28 +0530

    Add files via upload

---
+++ Graded Assignment - 1 - oops/src/com/greatlearning/main1/DriverClass.java
@@ -0,0 +1,19 @@
+package com.greatlearning.main1;
+
+public class DriverClass
+{
+
+	public static void main(String[] args)
+	{
+		SuperDepartment sd=new AdminDepartment();
+		SuperDepartment sd1=new HRDepartment();
+	    SuperDepartment sd2=new TechDepartment();
+		
+           sd.display();
+           System.out.println();
+           sd1.display();
+           System.out.println();
+           sd2.display();
+	}
+
+}
---
+++ Graded Assignment - 1 - oops/src/com/greatlearning/main1/SuperDepartment.java
@@ -0,0 +1,17 @@
+package com.greatlearning.main1;
+
+public class SuperDepartment {
+	String departmentName;
+	String getTodaysWork;
+	String getWorkDeadline;
+	String isTodayAHoliday;
+	
+	public void display()
+	{
+		System.out.println(departmentName= "Welcome to Super Department");
+		System.out.println(getTodaysWork= " No Work as of now");
+		System.out.println(getWorkDeadline= "Nil ");
+		System.out.println(isTodayAHoliday= "Today is a holiday");
+	}
+
+}
